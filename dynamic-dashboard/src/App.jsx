8677983fd55cb4ab444d07cb7c3d789aa517ca7c
nvp85 <nvp@zhukov.al>
2025-05-19 00:18:45 -0500

    create the dynamic dashboard

---
+++ dynamic-dashboard/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import DynamicDashboard from './components/DynamicDashboard';
+
+function App() {
+  return (
+    <>
+      <DynamicDashboard />
+    </>
+  )
+}
+
+export default App
---
+++ dynamic-dashboard/src/components/DynamicDashboard.jsx
@@ -0,0 +1,42 @@
+
+
+const DynamicDashboard = () => {
+    const isPremium = true;
+    const userName = "Jane Doe";
+    const today = new Date().toLocaleDateString();
+    const tasks = [
+        {
+            taskName: "Learn React",
+            completed: true
+        },
+        {
+            taskName: "Build a project",
+            completed: false
+        },
+        {
+            taskName: "Read documentation",
+            completed: true
+        }
+    ]
+    const completedTasks = tasks.filter(task=>task.completed).length;
+    const incompleteTasks = tasks.length - completedTasks;
+    return (
+        <div id="container">  
+            <h1>Hello, {userName}!</h1>
+            <p>Today is {today}</p>
+            {isPremium
+            ? <p>Thank you for being a premium member!</p>
+            : <p>Upgrade to premium to enjoy exclusive features!</p>
+            }
+            <h3>Task List:</h3>
+            <ul>
+                {tasks.map(task => <li>{task.taskName} {task.completed ? "✅" : "❌"}</li>)}
+            </ul>
+            <p>Completed tasks: {completedTasks}</p>
+            <p>Incomplete tasks: {incompleteTasks}</p>
+        </div>
+
+    )
+}
+
+export default DynamicDashboard;
